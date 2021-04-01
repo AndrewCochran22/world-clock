9547983fcff6ea072ce46798d59eec3a4d7abed0
Andrew Cochran <acoch22@gmail.com>
2021-04-01 12:32:58 -0400

    added homepage

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <form>
+          <label>
+            Timezones:
+            <select value="" onChange="">
+              <option value="" disabled>Select Timezone</option>
+            </select>
+          </label>
+
+        </form>
+        </div>
+    )
+}
